3ff89a6e969bded58adb977a723c11b1c710df27
Maxime LEBRETON <maxime.lebreton@etu.univ-poitiers.fr>
2023-02-25 15:10:24 +0100

    add csv file, dependency and way to read it in MainActivity

--- Pokinator/app/src/main/java/fr/aelysya/pokinator/MainActivity.java
+++ Pokinator/app/src/main/java/fr/aelysya/pokinator/MainActivity.java
@@ -3,6 +3,13 @@ package fr.aelysya.pokinator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.opencsv.CSVReader;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            CSVReader reader = new CSVReader(new InputStreamReader(getResources().openRawResource(R.raw.pokemon)));
+            String[] nextLine;
+            nextLine = reader.readNext();
+            Log.d("POKINATOR", nextLine[10]);
+        } catch (IOException e) {
+            Log.d("POKINATOR", "File not found.");
+        }
     }
 }

fd1f7e3b2547106cdac6e1a52f34169fb4ce8ac4
Maxime LEBRETON <maxime.lebreton@etu.univ-poitiers.fr>
2023-03-02 16:17:38 +0100

    began designing UI, added many resources

--- Pokinator/app/src/main/java/fr/aelysya/pokinator/MainActivity.java
+++ Pokinator/app/src/main/java/fr/aelysya/pokinator/MainActivity.java
@@ -4,21 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 
 import com.opencsv.CSVReader;
 
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String APP_TAG = "Pokinator";
+    public static boolean isXavier;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        isXavier = true;
+
+        ImageView logo = findViewById(R.id.logo);
+
+        logo.setOnClickListener(view -> {
+            logo.setImageResource(isXavier ? R.drawable.pokemon_hakim_clear : R.drawable.pokemon_xavier_clear);
+            isXavier = !isXavier;
+        });
 
         try {
             CSVReader reader = new CSVReader(new InputStreamReader(getResources().openRawResource(R.raw.pokemon)));

d3ea9dc05b6630e4f8b25c21470e64255d2d8a54
Maxime LEBRETON <maxime.lebreton@etu.univ-poitiers.fr>
2023-03-06 17:36:47 +0100

    Added size, weight, experience and HP filters

--- Pokinator/app/src/main/java/fr/aelysya/pokinator/utility/PokemonsData.java
+++ Pokinator/app/src/main/java/fr/aelysya/pokinator/utility/PokemonsData.java
@@ -164,12 +164,16 @@ public class PokemonsData {
             filterPreferredType("ghost");
             filterGeneration(new int[]{1, 6, 9});
             filterDislikedType("grass");
-            filterAtkDef(false, false);
-            filterSpeed(true);
-            filterCaptureRate(false);
-            filterEggSteps(true);
-            for(String s : NAMES){
-                Log.d("DEBUG", s);
+            filterSize(55);
+            //filterWeight(50);
+            //filterHP(false);
+            //filterAtkDef(false, false);
+            //filterSpeed(true);
+            //filterCaptureRate(false);
+            //filterEggSteps(true);
+            //filterMaxExperience(false);
+            for(int i = 0; i < NAMES.size(); ++i){
+                Log.d("DEBUG", NAMES.get(i) + " " + SIZES.get(i) + " " + WEIGHTS.get(i));
             }
 
 
@@ -257,19 +261,42 @@ public class PokemonsData {
         return TYPES.indexOf(type);
     }
 
+    /** Filter the dataset based on the pokémons HP, keeps the ones with the most or the least amount of HP
+     * @param keepLow Whether to keep the pokémons with the most or the least amount of HP
+     */
+    private void filterHP(boolean keepLow){
+        Log.d("Data filter", "HP stat filter begin, number of pokémons before: " + STATS.size());
+        int highestHp = 0;
+        int lowestHp = 1000;
+        for(Integer[] i : STATS){
+            highestHp = Math.max(highestHp, i[0]);
+            lowestHp = Math.min(lowestHp, i[0]);
+        }
+
+        int thirdsThreshold = (highestHp - lowestHp) / 3;
+
+        //Not using a forEach to track the index automatically
+        for(int i = 0; i < STATS.size(); ++i){
+            if((keepLow && STATS.get(i)[0] > highestHp - thirdsThreshold) || (!keepLow && STATS.get(i)[0] < lowestHp + thirdsThreshold)){
+                removeLine(i);
+                i--; //Rectify the iterator position
+            }
+        }
+        Log.d("Data filter", "HP stat filter end, number of pokémons left: " + STATS.size());
+    }
+
     /** Filter the dataset based on the pokémons attacks and defenses, keeps the ones that fit the user's choices
      * @param checkSpecial Whether to compare special or physical stats
      * @param prefAttack Whether to keep pokémons with a higher attack than defense or the contrary
      */
     private void filterAtkDef(boolean checkSpecial, boolean prefAttack){
         Log.d("Data filter", "Attacks and defenses stat filter begin, number of pokémons before: " + STATS.size());
-
         //Not using a forEach to track the index automatically
         for(int i = 0; i < STATS.size(); ++i){
-            if((checkSpecial && prefAttack && (STATS.get(i)[4] > STATS.get(i)[3])) //special and prefer SP-ATK and (SP-DEF > SP-ATK)
-            || (checkSpecial && !prefAttack && (STATS.get(i)[4] < STATS.get(i)[3])) //special and prefer SP-DEF and (SP-ATK > SP-DEF)
-            || (!checkSpecial && prefAttack && (STATS.get(i)[2] > STATS.get(i)[1])) //physical and prefer ATK and (DEF > ATK)
-            || (!checkSpecial && !prefAttack && (STATS.get(i)[2] < STATS.get(i)[1])) //physical and prefer DEF and (ATK > DEF)
+            if((checkSpecial && prefAttack && STATS.get(i)[4] > STATS.get(i)[3]) //special and prefer SP-ATK and (SP-DEF > SP-ATK)
+            || (checkSpecial && !prefAttack && STATS.get(i)[4] < STATS.get(i)[3]) //special and prefer SP-DEF and (SP-ATK > SP-DEF)
+            || (!checkSpecial && prefAttack && STATS.get(i)[2] > STATS.get(i)[1]) //physical and prefer ATK and (DEF > ATK)
+            || (!checkSpecial && !prefAttack && STATS.get(i)[2] < STATS.get(i)[1]) //physical and prefer DEF and (ATK > DEF)
             ){
                 removeLine(i);
                 i--; //Rectify the iterator position
@@ -300,6 +327,86 @@ public class PokemonsData {
             }
         }
         Log.d("Data filter", "Speed stat filter end, number of pokémons left: " + STATS.size());
+    }
+
+    /** Filter the dataset based on the pokémons' size, will keep only the pokémons that
+     * share the height that's the closest to the parameter value
+     * @param approximateValue Which pokémon size is wanted
+     */
+    public void filterSize(int approximateValue){
+        Log.d("Data filter", "Size filter begin, number of pokémons before: " + SIZES.size());
+        float tallest = 0;
+        float smallest = 1000;
+        for(Float f : SIZES){
+            tallest = Math.max(tallest, f);
+            smallest = Math.min(smallest, f);
+        }
+
+        //Get a representation of the height value from the seekbar value chosen by the user
+        float targetValue = smallest + ((float) approximateValue / 100 * (tallest - smallest));
+
+        float minimumHeightDifference = 1000;
+        float foundHeight = 1000;
+
+        //Find the closest height to the target
+        for(Float f : SIZES){
+            float tempDiff = Math.min(minimumHeightDifference, Math.abs(f - targetValue));
+            //If the read value is closer to the target than the previous one
+            if(tempDiff < minimumHeightDifference){
+                minimumHeightDifference = tempDiff;
+                foundHeight = f;
+            }
+        }
+
+        //Not using a forEach to track the index automatically
+        for(int i = 0; i < SIZES.size(); ++i){
+            //If the pokémon's height is different from the target, remove it from the dataset
+            if(SIZES.get(i) != foundHeight){
+                removeLine(i);
+                i--; //Rectify the iterator position
+            }
+        }
+        Log.d("Data filter", "Size filter end, number of pokémons left: " + SIZES.size());
+    }
+
+    /** Filter the dataset based on the pokémons' weight, will keep only the pokémons that
+     * share the weight that's the closest to the parameter value
+     * @param approximateValue Which pokémon weight is wanted
+     */
+    public void filterWeight(int approximateValue){
+        Log.d("Data filter", "Weight filter begin, number of pokémons before: " + WEIGHTS.size());
+        float lightest = 0;
+        float heaviest = 1000;
+        for(Float f : WEIGHTS){
+            lightest = Math.max(lightest, f);
+            heaviest = Math.min(heaviest, f);
+        }
+
+        //Get a representation of the weight value from the seekbar value chosen by the user
+        float targetValue = heaviest + ((float) approximateValue / 100 * (lightest - heaviest));
+
+        float minimumWeightDifference = 10000;
+        float foundWeight = 10000;
+
+        //Find the closest weight to the target
+        for(Float f : WEIGHTS){
+            float tempDiff = Math.min(minimumWeightDifference, Math.abs(f - targetValue));
+            //If the read value is closer to the target than the previous one
+            if(tempDiff < minimumWeightDifference){
+                minimumWeightDifference = tempDiff;
+                foundWeight = f;
+            }
+        }
+
+        //Not using a forEach to track the index automatically
+        for(int i = 0; i < WEIGHTS.size(); ++i){
+            //If the pokémon's weight is different from the target, remove it from the dataset
+            if(WEIGHTS.get(i) != foundWeight){
+                removeLine(i);
+                i--; //Rectify the iterator position
+            }
+        }
+        Log.d("Data filter", "Weight filter end, number of pokémons left: " + WEIGHTS.size());
     }
 
     /** Filter the dataset based on the pokémons' legendary status
@@ -366,5 +473,29 @@ public class PokemonsData {
             }
         }
         Log.d("Data filter", "Capture rate filter end, number of pokémons left: " + CAPTURE_RATES.size());
+    }
+
+    /** Filter the dataset based on the pokémons max experience, keeps the longest or the fastest to level up
+     * @param keepLong Whether to keep the longest or the fastest pokémons to level up
+     */
+    private void filterMaxExperience(boolean keepLong){
+        Log.d("Data filter", "Max experience filter begin, number of pokémons before: " + MAX_EXPERIENCE.size());
+        int highestXp = 0;
+        int lowestXp = 2000000;
+        for(Integer i : MAX_EXPERIENCE){
+            highestXp = Math.max(highestXp, i);
+            lowestXp = Math.min(lowestXp, i);
+        }
+
+        if(lowestXp != highestXp){
+            //Not using a forEach to track the index automatically
+            for(int i = 0; i < MAX_EXPERIENCE.size(); ++i){
+                if((keepLong && MAX_EXPERIENCE.get(i) == lowestXp) || (!keepLong && MAX_EXPERIENCE.get(i) == highestXp)){
+                    removeLine(i);
+                    i--; //Rectify the iterator position
+                }
+            }
+        }
+        Log.d("Data filter", "Max experience filter end, number of pokémons left: " + MAX_EXPERIENCE.size());
     }
 }

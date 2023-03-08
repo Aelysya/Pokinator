5e63793ad133e17cb61aae20e91eee4dde449ced
Maxime LEBRETON <maxime.lebreton@etu.univ-poitiers.fr>
2023-03-08 19:14:09 +0100

    Made all type background in 2 variations to combine them depending on later user's choices, added a way to change the background programatically

--- Pokinator/app/src/main/java/fr/aelysya/pokinator/TypesActivity.java
+++ Pokinator/app/src/main/java/fr/aelysya/pokinator/TypesActivity.java
@@ -1,11 +1,17 @@
 package fr.aelysya.pokinator;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import java.util.Objects;
 
 import fr.aelysya.pokinator.utility.PokemonsData;
 
@@ -21,6 +27,7 @@ public class TypesActivity extends AppCompatActivity {
 
         Button previousStep = findViewById(R.id.previousStepTypes);
         Button nextStep = findViewById(R.id.nextStepTypes);
+        Button test = findViewById(R.id.test);
 
         previousStep.setOnClickListener(view -> {
             Intent intent = new Intent(this, PreferencesActivity.class);
@@ -30,6 +37,21 @@ public class TypesActivity extends AppCompatActivity {
             //Filter the data
 
             data.logData();
+        });
+
+        //Example to change the background for other types
+        test.setOnClickListener(view -> {
+            //Find the xml background
+            LayerDrawable layerDrawable = (LayerDrawable) ContextCompat.getDrawable(this, R.drawable.type_background);
+            assert layerDrawable != null;
+
+            //Change the top and bottom parts by their id
+            layerDrawable.setDrawableByLayerId(R.id.topBack, getResources().getDrawable(R.drawable.fire_top));
+            layerDrawable.setDrawableByLayerId(R.id.bottomBack, getResources().getDrawable(R.drawable.ice_bottom));
+
+            //Set the new image
+            ImageView typeBack = findViewById(R.id.backgroundType);
+            typeBack.setImageDrawable(layerDrawable);
         });
     }
 
